9c3922a9a748b3661b78112bdaeeb34c421f7f19
Andrey Zheleznov <zheland.net@gmail.com>
2020-04-25 14:43:04 +0300

    Fix ts error: "An import path cannot end with a '.ts' extension"
    An import path cannot end with a '.ts' extension.
    Consider importing './index' instead.

--- www/ts/bootstrap.ts
+++ www/ts/bootstrap.ts
@@ -3,5 +3,5 @@ require("../less/styles.less");
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.ts` file does the single async import, so
 // that no one else needs to worry about it again.
-import("./index.ts")
+import("./index")
   .catch(e => console.error("Error importing `index.ts`:", e));
